3d6c8c8fcedf1ea544ca1afa4bd7fa421775da3c
felamaslen <felamaslen@gmail.com>
2014-07-28 13:44:57 +0100

    Added map

--- index.php
+++ index.php
@@ -4,14 +4,20 @@
     <title>Intersection (working title)</title>
     <link rel="stylesheet" href="css/style.css">
     <script src="js/jquery.min.js"></script>
+    <script type="text/javascript"
+      src="http://maps.googleapis.com/maps/api/js?key=AIzaSyByQNXD_ayeApNf_LSuZDYWcoSSA8gQWto"></script>
     <script src="js/intersection.js"></script>
   </head>
   <body>
     <div id="wrap">
-      <div id="map"></div>
+      <div id="map_outer">
+        <div id="map_inner">
+          <div id="map"></div>
+        </div>
+      </div>
       <div id="ctrl">
         <div class="inside">
-          
+        fasfdaflkjslksdflkjsalfsad;lfjsa;flkajf;lakj
         </div>
       </div>
     </div>
--- js/intersection.js
+++ js/intersection.js
@@ -5,7 +5,16 @@
  */
 
 (function($){
-  $(document).ready(function(){
+  function map_init() {
+    var options = {
+      center: new google.maps.LatLng(54.7, -3),
+      zoom: 6
+    };
+    var map = new google.maps.Map(document.getElementById("map"), options);
+  }
+  google.maps.event.addDomListener(window, 'load', map_init);
 
+  $(document).ready(function(){
+    
   });
 })(jQuery);
