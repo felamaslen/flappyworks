89689e1ca3828b445a933d411ec365cd3791760d
felamaslen <felamaslen@gmail.com>
2014-07-28 13:15:03 +0100

    Intersection base
    Base code for the main program

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Intersection (working title)</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/intersection.js"></script>
+  </head>
+  <body>
+    <div id="wrap">
+      <div id="map"></div>
+      <div id="ctrl">
+        <div class="inside">
+          
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ js/intersection.js
@@ -0,0 +1,11 @@
+/**
+ * @file js/intersection.js
+ * Copyright (c) 2014,
+ * Fela, Jacob, Saul, Malachy
+ */
+
+(function($){
+  $(document).ready(function(){
+
+  });
+})(jQuery);
