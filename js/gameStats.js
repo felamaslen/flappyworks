de9e5ef08260641fbca8002be9f3d07cfa5bc006
Saulsmcouk <ssm8@hotmail.co.uk>
2014-07-29 15:50:40 +0100

    Added budget indicator

---
+++ js/gameStats.js
@@ -0,0 +1,2 @@
+var me = me || { balance: '345,000'}
+$('#js-budget').append(me.balance);
---
+++ js/setup.js
@@ -0,0 +1,14 @@
+var latlong;
+function setloc(loc){
+	switch{
+		case loc == plymouth:
+      latlong = plymouth;
+      break;
+    case loc == manchester:
+      latlong = manchester;
+      break;
+    case loc == london:
+      latlong = london;
+      break;
+	}
+}
