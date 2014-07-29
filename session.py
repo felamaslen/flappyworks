e02e6eba91196a50c000b8e21518af6f7ee8ad13
Malachy Hall Thorpe <MalHT@users.noreply.github.com>
2014-07-29 16:48:00 +0100

    Added python autokill script

--- session.py
+++ session.py
@@ -1,4 +1,28 @@
+import json
 from firebase import firebase
-firebase = firebase.FirebaseApplication('https://interception.firebaseio.com', None)
-result = firebase.get('', None)
-print result
+import time
+
+ka = {}
+
+while True:
+
+  fb = firebase.FirebaseApplication('https://interception.firebaseio.com', None)
+  result = fb.get('', None)
+  
+  for item in result:
+    if not (item in ka):
+      ka[item] = {'p1':0,'p2':0}
+    if "keepAlive" in result[item]['player1']:
+      if result[item]['player1']['keepAlive'] != ka[item]['p1']:
+        ka[item]['p1'] = result[item]['player1']['keepAlive']
+      else:
+        print "Need to kill session " + str(item)
+        fb.delete("/"+str(item),None)
+    if "player2" in result[item]:    
+      if "keepAlive" in result[item]['player2']:
+        if result[item]['player2']['keepAlive'] != ka[item]['p2']:
+          ka[item]['p2'] = result[item]['player2']['keepAlive']
+        else:
+          print "Need to kill session " + str(item)
+          fb.delete("/"+str(item), None)
+  time.sleep(30)
