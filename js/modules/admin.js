be0bff795b4e119cebd5e6bf68043b084b15a261
Naz <nazim.jamil-mir@digitaslbi.com>
2014-07-30 11:48:37 +0100

    Added require JS and modified a few files to be AMD friendly :)

---
+++ js/modules/admin.js
@@ -0,0 +1,37 @@
+﻿require([
+        'jquery',
+        'firebase'
+    ],
+    function(
+        $,
+        firebase
+    ) {
+
+        var fb = new Firebase("https://interception.firebaseio.com/");
+
+        fb.on('value', function (snapshot) {
+
+        $('.data').html('');
+
+        $.each( snapshot.val(), function( data, index ){
+
+        $('.data').append( '<li>' + index.name + ' - <button class="removeItem" data-ref="'+ data +'" >Remove Me!</button> </li>' );
+
+        });
+
+        }, function (errorObject) {
+        console.log('The read failed: ' + errorObject.code);
+        });
+
+        function killLobby( event ){
+        var ref = $(event.target).data('ref');
+        toDie = new Firebase('https://interception.firebaseio.com/'+ref)
+        toDie.set( null, function( data ){
+        console.log( data );
+        } );
+        }
+
+        // Event Handlers
+        $('body').on( 'click', '.removeItem', killLobby);
+
+});
