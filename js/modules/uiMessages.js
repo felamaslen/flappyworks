737d07a0c064720da960c111c2f6621305238f2c
Malachy Hall Thorpe <MalHT@users.noreply.github.com>
2014-07-31 15:50:25 +0100

    more fixes

--- js/modules/uiMessages.js
+++ js/modules/uiMessages.js
@@ -15,13 +15,15 @@ function(
     $("#dialog-modal-background").show();    
     $("#dialog-message-text").html(msg);    
     okButton = $('<button id="dialog-message-button" class="btn btn-default">OK</button>').on('click', function() { $("#dialog-modal-background").hide() });
-    $("#dialog-message-button-container").replaceWith(okButton);
+    $("#dialog-message-button-container").html(okButton);
     
   }
   
   this.slideUpDialog = function(msg) {
-    $("#slideup-message").effect('slide', { direction: 'up', mode: 'show' }, 500);
-    $("#slideup-message-text").html(msg);    
+    $("#slideup-message").show("slide",{direction: 'up'})
+    $("#slideup-message-text").html(msg + " ");
+    okButton = $('<a href="#">ok</a>').on('click', function() { $("#slideup-message").hide("slide",{direction: 'down'}) });
+    $("#slideup-message-text").append(okButton);
   }
   
 return this;
