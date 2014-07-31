876f5392aada03dc1639b8a08c8b9cc9987bcd02
Malachy Hall Thorpe <MalHT@users.noreply.github.com>
2014-07-31 12:55:14 +0100

    _main.js update

--- js/_main.js
+++ js/_main.js
@@ -4,31 +4,29 @@ require.config({
     urlArgs: "bust="+Math.floor(Math.random()*999999),
     
     paths : {
-        
-      jquery:     'libs/jquery.min',
-      bootstrap:  'libs/bootstrap.min',
-      jquerycookie: 'libs/jquery.cookie',
       
-      firebase:   'libs/firebase',
-//      firebase: 'https://cdn.firebase.com/js/client/1.0.15/firebase',
-
-//      maps:       'libs/googlemaps', // this is loaded asynchronously via the footer in index.html
-    
-
+      jquery:       'libs/jquery.min',
+      jqueryUI:     'libs/jquery-ui.min',
+      bootstrap:    'libs/bootstrap.min',
+      jquerycookie: 'libs/jquery.cookie',
+      firebase:     'libs/firebase',
+    //firebase:     'https://cdn.firebase.com/js/client/1.0.15/firebase',
+    //maps:         'libs/googlemaps', // this is loaded asynchronously via the footer in index.html
       intersection: 'modules/intersection',
-      view:       'modules/view',
-      admin:      'modules/admin',
-      global:     'modules/global',
-      mapMethods: 'modules/mapMethods',
-      mapstuff_fela: 'modules/mapstuff_fela',
-      sync:       'modules/sync',
-      units: 'modules/units',
-      //unitPopulation: 'modules/unitPopulation'
+      view:         'modules/view',
+      admin:        'modules/admin',
+      global:       'modules/global',
+      mapMethods:   'modules/mapMethods',
+      mapstuff_fela:  'modules/mapstuff_fela',
+      sync:         'modules/sync',
+      units:        'modules/units',
+      uiMessages:     'modules/uiMessages',
 
     },
 
     shim: {
       'jquery': {exports: '$'},
+      'jqueryUI': { deps: ['jquery'] /*, exports: '$'*/ },
       'bootstrap': {deps:['jquery'], exports: 'bootstrap'},
       'jquerycookie' : {deps:['jquery'], exports: 'jquerycookie'},
       'firebase' : {deps:[], exports: 'Firebase'},
@@ -37,19 +35,30 @@ require.config({
 
 require([
         'jquery',
+        'global',
         'intersection',
-        //'unitPopulation',
         'units',
+        'uiMessages'
     ],
     function(
         $,
-        intersection
+        global,
+        intersection,
+        units,
+        uiMessages
     ) {
 
         $('document').ready(function(){
 
-            window.console && console.log('REQUIRE::Loaded');
-
+          window.console && console.log('REQUIRE::Loaded');        
+         
+          uiMessages.modalDialog("hi.");
+         
+          global.debug("test",1)
+          global.debug("test",3)
+          
+          
+           
         });
 
 });
