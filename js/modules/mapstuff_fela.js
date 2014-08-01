bd395e54b7e904a10d3f8acac531c38b2c885ab7
JMAlego <jacob@davidallenconsultancy.co.uk>
2014-08-01 23:13:18 +0100

    working touch events

--- js/modules/mapstuff_fela.js
+++ js/modules/mapstuff_fela.js
@@ -53,6 +53,9 @@ define([
         }
 
         global.evDragStart = function(e) {
+          e.preventDefault();
+          e.stopPropagation();
+        
           if (global.G == null) return false;
           var $target = $(e.target);
           
@@ -97,16 +100,20 @@ define([
           e.stopPropagation();
         
           var unit = global.G.dragData;
-
+          if (e.originalEvent.clientX && e.originalEvent.clientY){
+              var cX = e.originalEvent.clientX,
+              cY = e.originalEvent.clientY;
+          }else{
+              var cX = e.originalEvent.changedTouches[0].clientX,
+              cY = e.originalEvent.changedTouches[0].clientY;
+          }
           // find the coordinates of the click
-          var cX = e.originalEvent.clientX,
-              cY = e.originalEvent.clientY,
-
-              pos = global.$d.map_outer.position(),
+          var pos = global.$d.map_outer.position(),
               pX = cX - pos.left,
               pY = cY - pos.top;
           
-          console.log(e);
+          console.log(e.originalEvent.clientX);
+          console.log(e.originalEvent.changedTouches[0].clientX);
 
           var position = global.G.overlay.getProjection().fromContainerPixelToLatLng(new google.maps.Point(pX, pY)),
               lat = position.lat(),
